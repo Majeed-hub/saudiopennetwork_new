221b763cf98e344a626317a1e6f30449c9991a70
Majeed-hub <majeedbkl65@gmail.com>
2024-10-15 01:18:33 +0530

    added new file .nvmrc

--- src/containers/skills/FullStackImg.js
+++ src/containers/skills/FullStackImg.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import beckn from "../../assests/images/beckn_image.png"
 
 export default class FullStackImg extends Component {
   render() {
     const theme = this.props.theme;
     return (
-      <></>
+      <>
+      <img src={beckn} alt="" /></>
     );
   }
 }
