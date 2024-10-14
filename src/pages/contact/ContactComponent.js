ab62bab1aadb267bfb8e9f90da29c72dbcc3d1ea
Majeed-hub <majeedbkl65@gmail.com>
2024-10-15 02:24:36 +0530

    changed footer

--- src/pages/contact/ContactComponent.js
+++ src/pages/contact/ContactComponent.js
@@ -2,18 +2,12 @@ import React, { Component } from "react";
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import TopButton from "../../components/topButton/TopButton";
-import SocialMedia from "../../components/socialMedia/SocialMedia";
-import Button from "../../components/button/Button";
-import BlogsImg from "./BlogsImg";
-import AddressImg from "./AddressImg";
 import { Fade } from "react-reveal";
 import "./ContactComponent.css";
-import { greeting, contactPageData } from "../../portfolio.js";
+import { contactPageData } from "../../portfolio.js";
 
 const ContactData = contactPageData.contactSection;
-const blogSection = contactPageData.blogSection;
-const addressSection = contactPageData.addressSection;
-const phoneSection = contactPageData.phoneSection;
+
 
 class Contact extends Component {
   componentDidMount() {
